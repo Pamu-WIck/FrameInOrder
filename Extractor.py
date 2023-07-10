38a8fd2497c5dbae7e684e91b9e28237a91f4272
Pamu-Wick <pamudithawm@gmail.com>
2023-07-10 19:45:08 +0530

    added uploader.py

--- Extractor.py
+++ Extractor.py
@@ -1,38 +1,41 @@
 import cv2
-from datetime import datetime
 
-# Open the video file
-video_path = "video.mp4"
-cap = cv2.VideoCapture(video_path)
 
-# Check if the video file was opened successfully
-if not cap.isOpened():
-    print("Error opening video file")
-    exit()
+def extractor(name, frame_rate):
+    # Open the video file
+    video_path = "vid/" + name
+    cap = cv2.VideoCapture(video_path)
 
-frame_count = 0
-frame_rate = 3  # Desired frame rate (frames per second)
-interval = round(cap.get(cv2.CAP_PROP_FPS) / frame_rate)  # Calculate the frame interval
+    # Check if the video file was opened successfully
+    if not cap.isOpened():
+        print("Error opening video file")
+        exit()
 
-while True:
-    # Read the next frame from the video
-    ret, frame = cap.read()
+    frame_count = frame_rate  # Start with frame number 1
+    frame_rate = 8  # Desired frame rate (frames per second)
+    interval = round(cap.get(cv2.CAP_PROP_FPS) / frame_rate)  # Calculate the frame interval
+    frame_num = 1  # Start with frame number 1
 
-    # If the frame was not read successfully, then we have reached the end of the video
-    if not ret:
-        break
+    while True:
+        # Read the next frame from the video
+        ret, frame = cap.read()
 
-    # Check if the frame count matches the desired interval
-    if frame_count % interval == 0:
-        # Get the current time
-        current_time = datetime.now().strftime("%M%S%f")[:-3]
+        # If the frame was not read successfully, then we have reached the end of the video
+        if not ret:
+            break
 
-        # Save the frame as an image file with the current time in the name
-        frame_path = f"vid/frame_{current_time}_{frame_count}.jpg"
-        cv2.imwrite(frame_path, frame)
+        # Check if the frame count matches the desired interval
+        if frame_count % interval == 0:
+            # Save the frame as an image file with sequential numbering
+            frame_path = f"frames/{frame_num}.jpg"
+            cv2.imwrite(frame_path, frame)
+            frame_num += 1  # Increment the frame number
 
-    # Increment the frame counter
-    frame_count += 1
+        # Increment the frame counter
+        frame_count += 1
 
-# Release the video file
-cap.release()
+    # Release the video file
+    cap.release()
+
+
+extractor("cat.mp4", 1)
--- uploader.py
+++ uploader.py
@@ -3,29 +3,39 @@ import time
 import os
 import random
 
-token = 'token here'
-fb = facebook.GraphAPI(access_token=token)
 
-profile = fb.get_object(id=102750666053085, fields='feed', limit='100')
-# latestMessage = profile['feed']['data'][0]['message']
-# latestFrame = int(latestMessage.split(" ")[-4])
+def uploader(token, title, currentframe, length):
+    token = token
+    fb = facebook.GraphAPI(access_token=token)
 
-title = "Title"
-currentFrame = 647
-length = 1989
+    # profile = fb.get_object(id=102750666053085, fields='feed', limit='100')
+    # latestMessage = profile['feed']['data'][0]['message']
+    # latestFrame = int(latestMessage.split(" ")[-4])
 
-print(currentFrame)
+    title = title
+    currentFrame = currentframe
+    length = length
 
-if not os.path.isdir('./frames'):
-    os.mkdir('./frames')
+    print(currentFrame)
 
-while currentFrame <= length:
-    imagePath = './frames/' + str(currentFrame) + '.jpg'
-    image = open(imagePath, 'rb')
-    fb.put_photo(image=image, message=title + " Frame " + str(currentFrame) + " out of " + str(length))
-    print("Uploaded " + str(currentFrame))
-    time.sleep(random.choice([60,120]))
+    if not os.path.isdir('./frames'):
+        os.mkdir('./frames')
 
-    currentFrame += 1
+    while currentFrame <= length:
+        imagePath = './frames/' + str(currentFrame) + '.jpg'
+        image = open(imagePath, 'rb')
+        fb.put_photo(image=image, message=title + "\nFrame " + str(currentFrame) + " out of " + str(length))
+        print("Uploaded " + str(currentFrame))
+        time.sleep(random.choice([60, 5]))
 
-time.sleep(86400)
+        currentFrame += 1
+
+    time.sleep(86400)
+
+
+uploader(
+    'EAAHyLBJeZB38BAAZCY3gee5J0jspLRsCQvjv9UtKIqMuCTNZALqZCtRVHdPXcsLHJ12cr9ghjHUkTMa7YgqEB4sqYEYdshZA9NXyjhDtXhWDrsL0Q5ViOvGgEu6y6PXf7WO7uWun5K17RvxgKaQeiNM3qlNcwWN9jXbiuuLJ1TrZACy3IDSdOp',
+    'Suratha - IRAJ Ft. Killer B & Kaizer',
+    1,
+    2448
+)
